a4449407a559381e6db166bbeee05d30b88fab74
Carl <monnaert.carl@gmail.com>
2026-09-12 23:08:16 +0200

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxAreaSlow(self, heights: List[int]) -> int:
+        h = {(i,j) : 0 for i in range(len(heights)) for j in range(len(heights))}
+        for i,j in h.keys():
+            h[(i,j)] = min(heights[i], heights[j]) * abs(i - j)
+        return max(h.values())
+
+    def maxArea(self, heights: List[int]) -> int:
+        start = 0
+        end = len(heights) - 1
+        M = min(heights[start], heights[end]) * (end - start)
+        while start < end:
+            if M < min(heights[start], heights[end]) * (end - start):
+                M = min(heights[start], heights[end]) * (end - start)
+            
+            if heights[start] < heights[end]:
+                start += 1
+            else:
+                end -= 1
+        return M
